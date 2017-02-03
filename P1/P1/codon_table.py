7c9b644fac2d856a4c15bced3a52e0851b96325d
Laura Kaitlin Hill Donohue <lkhd@corn15.stanford.edu>
2017-02-03 10:10:22 -0800

    Remove P1

--- P1/P1/codon_table.py
+++
@@ -1,7 +0,0 @@
-codon_table_dict = {'GAA': 'glutamine', 'GAG': 'glutamine', 'AAA': 'lysine', 'AAG': 'lysine', 'CCU': 'proline', 'CCC': 'proline', 'CCA': 'proline', 'CCG': 'proline'}
-
-amino_acid = input("Enter a codon:").upper()
-
-result = (codon_table_dict[amino_acid])
-
-print (amino_acid , result)
